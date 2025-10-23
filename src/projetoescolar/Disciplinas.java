112a808263b17f5dfa1a48c700d41a29e5ba4546
williamRamosM <williamramosmoreira01@gmail.com>
2025-10-23 01:35:54 -0300

    atualizado diciplinas

--- src/projetoescolar/Disciplinas.java
+++ src/projetoescolar/Disciplinas.java
@@ -1,11 +1,18 @@
-public class Disciplinas {
+import java.util.ArrayList;
 
+public class Disciplinas {
+    private static ArrayList <Disciplinas> arrayL = new ArrayList<>();
     private String id, nomeM, conteudo;
 
-    public void Diciplinas(String id, String nomeM, String conteudo){
+
+
+    public Disciplinas(String id, String nomeM, String conteudo){
         this.id = id;
         this.nomeM = nomeM;
         this.conteudo = conteudo;
+    }
+
+    public  Disciplinas() {
     }
 
     public void setId(String id) {
@@ -32,11 +39,67 @@ public class Disciplinas {
         return conteudo;
     }
 
+    public void add (Disciplinas disciplinas){
+        arrayL.add(disciplinas);
+    }
+
+    public String list(){
+        String text = "";
+
+        for(int i = 0; i < arrayL.size(); i++){
+            text = text + arrayL.get(i).toString();
+        }
+
+        return text;
+    }
+
+    public Disciplinas buscar(String id) {
+
+        for(int i = 0; i < arrayL.size(); i++){
+            Disciplinas disciplinas = arrayL.get(i);
+
+            if(disciplinas.getId().equalsIgnoreCase(id)){
+                return disciplinas;
+            }
+        }
+        return null;
+    }
+
+    public String delet(String id){
+
+        String text = "";
+        try {
+
+            if(buscar(id) != null) {
+
+                for (int i = 0; i < arrayL.size(); i++) {
+                    Disciplinas disciplinas = arrayL.get(i);
+
+                    if (disciplinas.getId().equalsIgnoreCase(id)) {
+                        arrayL.remove(i);
+                        text = "System > Materia retirada com sucesso! <";
+                    }
+                }
+            }
+            else{
+               text = "System > Materia nao existe para excluir! <";
+            }
+        }catch(Exception e){
+            System.out.println("ERRO -> "+e.getMessage());
+        }
+
+        return text;
+    }
+
     @Override
     public String toString() {
-        return "Disciplinas \n" +
+        return "\n" +
+                ">> Disciplinas <<\n" +
                 "id:" +id+ "\n" +
                 "nomeM: "+nomeM+"\n"+
-                "conteudo: "+conteudo;
+                "conteudo: "+conteudo+"\n\n";
     }
+
+
+
 }

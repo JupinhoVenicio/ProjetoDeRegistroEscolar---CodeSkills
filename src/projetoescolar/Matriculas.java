1304189e33838571fa1422782cf6eb89b0bb9e4c
TIAGO MAIER FERREIRA CARDOSO <2023326465@ALUNOS.OSORIO.IFRS.EDU.BR>
2025-10-27 10:50:59 -0300

    classe matriculadas finalizada

--- src/projetoescolar/Matriculas.java
+++ src/projetoescolar/Matriculas.java
@@ -8,7 +8,7 @@ public class Matriculas{
 
 Scanner Scanner = new Scanner(System.in);
 
-public static void menuMatriculas(ArrayList<String> professores, ArrayList<String> alunos, ArrayList<> disciplinas){
+public static void menuMatriculas(ArrayList<String> alunos, ArrayList<String> professor, ArrayList<> disciplinas, ArrayList<String>AlunosMatriculados, ArrayList<String>DisciplinasMatriculadas){
 
     int num = -1;
     do{
@@ -18,11 +18,60 @@ public static void menuMatriculas(ArrayList<String> professores, ArrayList<Strin
         System.out.println("2-")
         System.out.println("-----MENU DAS MATRICULAS-----")
         
+        switch (op) {
+                case 0:
+                    break;
+                case 1:
+                    adicionarMatriculas(alunos, professor, disciplinas, AlunosMatriculados, DisciplinasMatriculadas);
+                    break;
+                case 2:
+                    listarMatriculas(AlunosMatriculados, DisciplinasMatriculadas);
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+                    break;
+            }
 
-    }while(num=!0)
+    }while(num != 0)
 
 
 }
 
+ public void adicionarMatriculas(ArrayList<String>alunos, ArrayList<String>professor ArrayList<String>disciplinas, ArrayList<String>AlunosMatriculados, ArrayList<String>DisciplinasMatriculadas){
+        alunos.listarAluno(alunos);
+        System.out.println("Digite o número do aluno que deseja matricular:");
+        while(!teclado.hasNextInt()){
+            System.out.println("Entrada inválida! Por favor, digite um número.");
+            teclado.next();
+        }
+        int nAluno = Integer.parseInt(teclado.nextLine());
+
+        disciplinas.listarDisciplinas(disciplinas, professor);
+        System.out.println("Digite o número da disciplina que deseja matricular:");
+        while(!teclado.hasNextInt()){
+            System.out.println("Entrada inválida! Por favor, digite um número.");
+            teclado.next();
+        }
+        int nDisciplina = Integer.parseInt(teclado.nextLine());
+
+        if(nAluno < alunos.size() && nAluno >= 0 && nDisciplina < disciplinas.size() && nDisciplina >= 0){
+            AlunosMatriculados.add(alunos.get(nAluno));
+            DisciplinasMatriculadas.add(disciplinas.get(nDisciplina));
+            System.out.println("Matrícula realizada com sucesso!");
+        } else {
+            System.out.println("Número do aluno ou disciplina inválido!");
+        }
+    }
+
+    public void listarMatriculas(ArrayList<String>AlunosMatriculados, ArrayList<String>DisciplinasMatriculadas){
+        System.out.println("-----LISTA DE MATRÍCULAS-----");
+        if(AlunosMatriculados.isEmpty()){
+            System.out.println("Nenhuma matrícula cadastrada.");
+        } else {
+            for(int i = 0; i < AlunosMatriculados.size(); i++){
+                System.out.println("Aluno: " + AlunosMatriculados.get(i) + " - Disciplina: " + DisciplinasMatriculadas.get(i));
+            }
+        }
+    }
 
 }

45e2183b3209c61ff8a795da18652f3043d4fd5a
RICHARD VIEIRA DUARTE <2025305277@ALUNOS.OSORIO.IFRS.EDU.BR>
2025-10-27 10:39:26 -0300

    comeco

---
+++ src/projetoescolar/Matriculas.java
@@ -0,0 +1,28 @@
+package projetoescolar;
+
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+public class Matriculas{
+
+Scanner Scanner = new Scanner(System.in);
+
+public static void menuMatriculas(ArrayList<String> professores, ArrayList<String> alunos, ArrayList<> disciplinas){
+
+    int num = -1;
+    do{
+        System.out.println("\n-----MENU DAS MATRICULAS-----")
+        System.out.println("\n0-Sair")
+        System.out.println("1-Numero da Matricula")
+        System.out.println("2-")
+        System.out.println("-----MENU DAS MATRICULAS-----")
+        
+
+    }while(num=!0)
+
+
+}
+
+
+}
